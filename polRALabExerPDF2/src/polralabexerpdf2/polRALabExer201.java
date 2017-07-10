9efa790d616c47ffa9fff67fc7d0a21601b66667
Roman Tristan Pol <romantristanpol@gmail.com>
2017-07-10 18:15:01 +0800

    Added polRALabExerPDF2

---
+++ polRALabExerPDF2/src/polralabexerpdf2/polRALabExer201.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polralabexerpdf2;
+
+/**
+ *
+ * @author artipee
+ */
+public class polRALabExer201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] val = {0, 1, 2, 3};
+        int sum = val[0]+val[1]+val[2]+val[3];
+        System.out.println( "Sum of all numbers = " + sum );
+    }
+    
+}
---
+++ polRALabExerPDF2/src/polralabexerpdf2/polRALabExer202.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polralabexerpdf2;
+
+/**
+ *
+ * @author artipee
+ */
+public class polRALabExer202 {
+    public static void main(String[] args) {
+        int[] val = {13, -4, 82, 17};
+        int[] twice;
+        System.out.println( "Original Array: "
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+        // Construct an array object for twice.
+        // Put values in twice that are twice the
+        // corresponding values in val.
+        System.out.println( "New Array: "
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    }
+}
